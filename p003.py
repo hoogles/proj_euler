b43ab8b37a4966e385d82d619d52d2f816ac8670
Hart Ramsey <hooglese@gmail.com>
2020-08-31 14:12:10 -0600

    Create p003.py

---
+++ p003.py
@@ -0,0 +1,25 @@
+from math import ceil
+
+def FF(N): 
+    a = ceil(N**0.5)
+    b2 = a**2 - N
+    while int(b2**0.5)-b2**0.5 == 0:
+        a+=1
+        b2 = a**2-N
+
+    return a-b2**0.5
+    
+    #prime factorization sucks.
+def pfact(x):
+    if x%2==0:
+        x/=2
+    lhs = FF(x)
+    rhs = x/lhs
+    if lhs ==1:
+        return rhs
+    else:
+        lprime = pfact(lhs)
+        rprime = pfact(rhs)
+        return max([lprime, rprime])
+        
+print(pfact(600851475143))
