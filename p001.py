c4b06b3196056b857617503bb4f3b0548e045d23
Hart Ramsey <hooglese@gmail.com>
2020-08-31 12:32:52 -0600

    typo sum to sumab
    oopsiepoosie. Also added print to make it readable

--- p001.py
+++ p001.py
@@ -3,4 +3,6 @@ from math import ceil
 sumn = lambda n : (n*(n-1)/2)
 sumab = lambda a,b : sumn(ceil(a/b))*b
 
-sol = sumab(1000,3)+sum(1000,5) - sumab(1000,15) # O(1)
+sol = sumab(1000,3)+sumab(1000,5) - sumab(1000,15) # O(1)
+
+print(sol)
